347e75a9ba225e866aeaca18ccbaf6b386fbb188
SkylabMak <skylab135@gmail.com>
2023-03-14 16:10:21 +0700

    debug

--- App/object/FTile/Tile_Size2.java
+++ App/object/FTile/Tile_Size2.java
@@ -3,7 +3,7 @@ package App.object.FTile;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 
-public class Tile_Size2 extends Tile{
+public class Tile_Size2 extends Tile {
     public Tile_Size2(JPanel jPanel) {
         super(jPanel);
         btnTile.addMouseListener(new mouseEvent());

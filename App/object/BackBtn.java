82780243e37c14e312118e87691d9fa3b4b42613
SkylabMak <skylab135@gmail.com>
2023-03-17 15:55:48 +0700

    banner add Btn back

--- App/object/BackBtn.java
+++ App/object/BackBtn.java
@@ -3,29 +3,49 @@ package App.object;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
-public class BackBtn extends JButton implements ActionListener {
+public class BackBtn extends JButton {
     Stat stat;
 
     public BackBtn() {
-        setText("Back");
+        setText("Back02");
         stat = new Stat();
         setBounds(15, 15, 90, 40);
         setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        addMouseListener(new mouseEvent());
+
     }
 
-    // public void addActionListener(new ActionListener() {
+    public class mouseEvent extends MouseAdapter {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("nha hee");
+            stat.sTartgGameOvar();
+        }
+
+    }
     // @Override
     // public void actionPerformed(ActionEvent e) {
-    // stat.sTartgGameOvar();
     // System.out.println("nha hee");
+    // stat.sTartgGameOvar();
+    // throw new UnsupportedOperationException("Unimplemented method
+    // 'actionPerformed'");
     // }
-    // });
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        stat.sTartgGameOvar();
-        System.out.println("nha hee");
-    }
+
+    // private class BtnListener implements ActionListener {
+
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    // System.out.println("nha hee");
+    // stat.sTartgGameOvar();
+    // throw new UnsupportedOperationException("Unimplemented method
+    // 'actionPerformed'");
+    // }
+
+    // }
 }

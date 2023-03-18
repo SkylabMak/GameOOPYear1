92546639928380c475044b94a79bdbd7d1411593
unknown <lan2chx@gmail.com>
2023-03-18 14:39:27 +0700

    Pull  Nut

---
+++ App/object/FTile/Table.java
@@ -0,0 +1,30 @@
+package App.object.FTile;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Table {
+    private JFrame A;
+    private JButton press;
+    private JButton pressR;
+    private JButton pressL;
+
+    Table() {
+        detailComponect();
+    }
+
+    private void detailComponect() {
+        press = new JButton("");
+        pressL = new JButton("");
+        pressR = new JButton("");
+        press.setPreferredSize(new Dimension(500, 500));
+        pressL.setPreferredSize(new Dimension(500, 500));
+        pressR.setPreferredSize(new Dimension(500, 500));
+        A.setLayout(new FlowLayout());
+        A.add(press);
+        A.add(pressL);
+        A.add(pressR);
+    }
+}
